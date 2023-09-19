a6bfd3adb961b3452d9c5b704ade689a7e43d4ad
Zahar Fedorchuk <puzeblin888@gmail.com>
2023-09-19 20:19:56 +0300

    Initial commit

---
+++ src/main/java/User.java
@@ -0,0 +1,9 @@
+class User {
+    private String name;
+    private String lastName;
+
+    public User(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+}
